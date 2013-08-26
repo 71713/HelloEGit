868745984dabb1a2aa8db8c8e6ad49a6dff1211e
yuri <Yuri.Shitara@jrits.ricoh.co.jp>
2013-08-26 10:22:23 +0900

    2nd commit from master to dev

--- src/HelloEGit.java
+++ src/HelloEGit.java
@@ -4,6 +4,7 @@ public class HelloEGit {
 	public static void main(String[] args) {
 		System.out.println("Hello EGit!");
 		System.out.println("push devrepo");
+		System.out.println("2nd push");
 	}
 
 }
